3c9e8374cac2fc8755bdf1319641c49575773afe
Tee Johnson <johnsontee777@gmail.com>
2021-03-13 18:17:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+'''
+for in Loop
+
+'''
+# students=['Bob','Maria','Peter']
+# for x in students:
+#    print('hi',x)
+
+  #  ==================
+
+# print(range(10))
+# print(list(range(10)))
+# print(list(range(1,10,2)))
+
+# ======================
+# Nested for Loop
+
+# for x in range(6):
+#  print(x+1)
+
+# for i in range(1,6):
+#    for j in range(i):
+#      print('*', end=' ')
+#    print()
+
+# ======================
+'''
+While Loop
+'''
+
+# i=1
+# while i < 10:
+#   print(i)
+#   i += 1
+
+
+ #===========================
+#  break statement
+
+# i=1
+# while i < 10:
+#   print(i)
+#   if i == 3:
+#     break
+#   i += 1
+
+#======================
+# Continue statements
+
+i=1
+while i < 10:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+ 
